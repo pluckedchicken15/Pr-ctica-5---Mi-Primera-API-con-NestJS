a2de4fbdff2e5b903231ae764cb3899f74cb0eb1
pluckedchicken15 <andrea.melendrez252774@potros.itson.edu.mx>
2026-09-08 19:12:59 -0700

    cambios para pruebas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,16 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service.js';
 
 interface Clase{
   id: number;
   nombre: string;
+  hora: string;
+  instructor: string;
 }
 
 const clases: Clase[] = [
-  {id:1, nombre: "yoga"},
-  {id:2, nombre: "spinning"}
+  {id:1, nombre: "yoga", hora: "3:00pm", instructor: "Manuel Turizo"},
+  {id:2, nombre: "spinning", hora: "6:00pm", instructor: "Christian Nodal"}
 ]
 
 @Controller()
@@ -26,9 +28,41 @@ export class AppController {
   }
 
   @Post('clases')
-    crear(@Body() cuerpo: {nombre: string}): Clase{
-      const nueva: Clase = { id: clases.length + 1, nombre: cuerpo.nombre}
+    crear(@Body() cuerpo: {nombre: string, hora: string, instrutor: string}): Clase{
+      const nueva: Clase = { id: clases.length + 1, nombre: cuerpo.nombre, hora: cuerpo.hora, instructor: cuerpo.instrutor}
       clases.push(nueva);
       return nueva;
     }
+
+    @Delete('clases/:id')
+    eliminar(@Param('id') id: string): Clase | string {
+      const indice = clases.findIndex(c => c.id === +id);
+
+      if (indice === -1) {
+        return 'Clase no encontrada';
+      }
+
+      const eliminada = clases.splice(indice, 1);
+
+      return eliminada[0];
+    }
+
+    @Patch('clases/:id')
+    editar(
+      @Param('id') id: string,
+      @Body() cuerpo: { nombre: string }
+    ): Clase | string {
+
+      const clase = clases.find(c => c.id === +id);
+
+      if (!clase) {
+        return 'Clase no encontrada';
+      }
+
+      clase.nombre = cuerpo.nombre;
+
+      return clase;
+    }
+
+
 }
